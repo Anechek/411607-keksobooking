818947984568eaea4eee7f14180b945ade1c9e6e
Anechek <anechek1@yandex.ru>
2017-12-20 16:47:18 +0300

    Исправла замечания в задании №24 личный проект: перламутровые пуговицы

--- js/map.js
+++ js/map.js
@@ -7,6 +7,8 @@ window.map = (function () {
   var MIDDLE = 'middle';
   var LOW = 'low';
   var HIGH = 'high';
+  var PRICE_LOW_LIMIT = '10000';
+  var PRICE_HIGH_LIMIT = '50000';
 
   var mapPins;
 
@@ -55,73 +57,72 @@ window.map = (function () {
 
   // Функция для скрытия всех пинов
 
-  var hideAllPins = function () {
-    for (var i = 1; i < window.map.mapPins.length; i++) {
-      window.map.mapPins[i].setAttribute('hidden', '');
+  var hideAllPins = function (pins) {
+    for (var i = 1; i < pins.length; i++) {
+      pins[i].setAttribute('hidden', '');
     }
   };
 
-  // Функция для фильтрации массива объявлений, но не более max
+  var filterByType = function (element, filter) {
+    return (filter === ANY) || (filter === element.offer.type);
+  };
 
-  var getFilteredAdverts = function (max, filter1, filter2, filter3, filter4, filters5) {
-    var arr = window.data.adverts.
-        map(function (advert, index) {
-          advert.mainIndex = index;
-          return advert;
-        }).
-        filter(function (advert) {
-          return (filter1 === ANY) || (filter1 === advert.offer.type);
-        }).
-        filter(function (advert) {
-          switch (filter2) {
-            case ANY:
-              return true;
-            case MIDDLE:
-              return (advert.offer.price >= 10000) && (advert.offer.price <= 50000);
-            case LOW:
-              return (advert.offer.price < 10000);
-            case HIGH:
-              return (advert.offer.price > 50000);
-            default:
-              return false;
-          }
-        }).
-        filter(function (advert) {
-          return (filter3 === ANY) || (parseInt(filter3, 10) === advert.offer.rooms);
-        }).
-        filter(function (advert) {
-          return (filter4 === ANY) || (parseInt(filter4, 10) === advert.offer.guests);
-        }).
-        filter(function (advert) {
-          if (filters5.length === 0) {
-            return true;
-          } else {
-            var heap = ' ' + advert.offer.features.join(', ');
-            for (var i = 0; i < filters5.length; i++) {
-              if (heap.lastIndexOf(' ' + filters5[i]) < 0) {
-                return false;
-              }
-            }
-            return true;
-          }
-        }).
-        slice(0, max);
-    return arr;
+  var filterByPrice = function (element, filter) {
+    switch (filter) {
+      case ANY:
+        return true;
+      case MIDDLE:
+        return (element.offer.price >= PRICE_LOW_LIMIT) && (element.offer.price <= PRICE_HIGH_LIMIT);
+      case LOW:
+        return (element.offer.price < PRICE_LOW_LIMIT);
+      case HIGH:
+        return (element.offer.price > PRICE_HIGH_LIMIT);
+      default:
+        return false;
+    }
+  };
+
+  var filterByRooms = function (element, filter) {
+    return (filter === ANY) || (parseInt(filter, 10) === element.offer.rooms);
+  };
+
+  var filterByGuests = function (element, filter) {
+    return (filter === ANY) || (parseInt(filter, 10) === element.offer.guests);
+  };
+
+  var filterByFeatures = function (element, filters) {
+    if (filters.length === 0) {
+      return true;
+    } else {
+      var heap = ' ' + element.offer.features.join(', ');
+      for (var i = 0; i < filters.length; i++) {
+        if (heap.lastIndexOf(' ' + filters[i]) < 0) {
+          return false;
+        }
+      }
+      return true;
+    }
   };
 
   // Функция для отображения пинов
 
   var showPins = function (count, houseFilter, priceFilter, roomsFilter, guestsFiter, features) {
-
-    hideAllPins();
-    var filteredAdverts = getFilteredAdverts(count, houseFilter, priceFilter, roomsFilter, guestsFiter, features);
+    hideAllPins(mapPins);
+    var filteredAdverts = window.data.adverts.map(function (advert, index) {
+      advert.mainIndex = index;
+      return advert;
+    }).
+        filter(function (advert) {
+          return filterByType(advert, houseFilter) && filterByPrice(advert, priceFilter) && filterByRooms(advert, roomsFilter) && filterByGuests(advert, guestsFiter) && filterByFeatures(advert, features);
+        }).
+        slice(0, count);
 
     // Осталось только отобразить все отфильтрованные Пины
 
     filteredAdverts.forEach(function (element) {
-      for (var i = 1; i < window.map.mapPins.length; i++) {
-        if (parseInt(window.map.mapPins[i].dataset.num, 10) === element.mainIndex) {
-          window.map.mapPins[i].removeAttribute('hidden', '');
+      for (var i = 1; i < mapPins.length; i++) {
+        if (parseInt(mapPins[i].dataset.num, 10) === element.mainIndex) {
+          mapPins[i].removeAttribute('hidden', '');
         }
       }
     });
@@ -131,19 +132,18 @@ window.map = (function () {
 
   var onButtonMouseup = function (evt) {
     var clickedElement = evt.currentTarget;
+
     clickedElement.classList.add('map__pin--active');
     window.form.noticeForm.classList.remove('notice__form--disabled');
     window.showCard(null, window.card.articleTemp, false);
     window.pin.addMapPins(window.data.adverts);
-    window.map.mapPins = document.querySelectorAll('.map__pin');
-    window.pin.addHandlersForAllButtons();
+    mapPins = document.querySelectorAll('.map__pin');
+    window.pin.addHandlersForAllButtons(mapPins);
     window.form.makeActiveAllFields(true);
     document.querySelector('.map').classList.remove('map--faded');
     showPins(MAX_VISIBLE_PINS, ANY, ANY, ANY, ANY, []);
     mapPinMain.removeEventListener('mouseup', onButtonMouseup);
   };
-
-  var mapPinsBlock = document.querySelector('.map__pins');
 
   // Делаем обработчик для отпускания мышки на элементе .map__pin--main
 
@@ -155,9 +155,8 @@ window.map = (function () {
 
   return {
     MAX_VISIBLE_PINS: MAX_VISIBLE_PINS,
-    mapPinsBlock: mapPinsBlock,
+
     mapPinMain: mapPinMain,
-    mapPins: mapPins,
     showPins: showPins
   };
 })();
--- js/pin.js
+++ js/pin.js
@@ -3,8 +3,6 @@ window.pin = (function () {
   var MAP_PIN_WIDTH_HALF = 20;
   var MAP_PIN_HEIGHT_PLUS10 = 50;
   var ENTER_KEYCODE = 13;
-  var DEBOUNCE_INTERVAL = 500;
-  var lastTimeout;
 
   // Функция, определяющая действия при клике на mapPin
 
@@ -30,30 +28,18 @@ window.pin = (function () {
 
   // Обрабочик для кнопок
 
-  function addHandlersForAllButtons() {
-    for (var i = 0; i < window.map.mapPins.length; i++) {
-      window.map.mapPins[i].addEventListener('click', function (evt) {
+  function addHandlersForAllButtons(buttons) {
+    for (var i = 0; i < buttons.length; i++) {
+      buttons[i].addEventListener('click', function (evt) {
         onMapPinClick(evt);
       });
-      window.map.mapPins[i].addEventListener('keydown', function (evt) {
+      buttons[i].addEventListener('keydown', function (evt) {
         if (evt.keyCode === ENTER_KEYCODE) {
           onMapPinClick(evt);
         }
       });
     }
   }
-
-  // Функция для нахождения выбранного значения в селекте
-
-  var getSelectedOption = function (select) {
-    for (var i = 0; i < select.options.length; i++) {
-      var option = select.options[i];
-      if (option.selected) {
-        return option.value;
-      }
-    }
-    return null;
-  };
 
   // Функция получает массив нажатых инпутов в фиелдсете
 
@@ -67,44 +53,58 @@ window.pin = (function () {
     return arr;
   };
 
-  // Функция для задержки исполнения функции
-
-  var debounce = function (fun) {
-    if (lastTimeout) {
-      window.clearTimeout(lastTimeout);
-    }
-    lastTimeout = window.setTimeout(fun, DEBOUNCE_INTERVAL);
-  };
-
   // Функция для обработчика при любом изменении фильтра
 
   function onFilterChange() {
-    debounce(function () {
+    window.debounce(function () {
 
       // Подготавливаем переменные для фильтра
 
       var form = document.forms[0];
-      var houseType = getSelectedOption(form.elements['housing-type']);
-      var priceType = getSelectedOption(form.elements['housing-price']);
-      var rooms = getSelectedOption(form.elements['housing-rooms']);
-      var guests = getSelectedOption(form.elements['housing-guests']);
+      var houseType = form.elements['housing-type'].value;
+      var priceType = form.elements['housing-price'].value;
+      var rooms = form.elements['housing-rooms'].value;
+      var guests = form.elements['housing-guests'].value;
       var features = getCheckedInput(form.elements['housing-features'].elements);
 
       // Показываем только те пины, которые соответствуют фильтру 
 
       window.map.showPins(window.map.MAX_VISIBLE_PINS, houseType, priceType, rooms, guests, features);
 
-      // Если отфильтровался активный пин, то надо срыть карточку
+      // Если отфильтровался активный пин, то надо скрыть карточку
 
       var activePin = document.querySelector('.map__pin--active');
-      if (activePin !== null) {
-        if (activePin.hasAttribute('hidden')) {
-          window.showCard(null, window.card.articleTemp, false);
-          activePin.classList.remove('map__pin--active');
-        }
+
+      if (activePin !== null && activePin.hasAttribute('hidden')) {
+        window.showCard(null, window.card.articleTemp, false);
+        activePin.classList.remove('map__pin--active');
       }
     });
   }
+
+  // Функция для создания баттонов для каждого маппина
+
+  var addMapPins = function (advertsArray) {
+    var fragment = document.createDocumentFragment();
+    for (var i = 0; i < advertsArray.length; i++) {
+      var button = document.createElement('button');
+      button.style.left = '' + (advertsArray[i].location.x - MAP_PIN_WIDTH_HALF) + 'px';
+      button.style.top = '' + (advertsArray[i].location.y - MAP_PIN_HEIGHT_PLUS10) + 'px';
+      button.className = 'map__pin';
+      button.innerHTML = '<img src="' + advertsArray[i].author.avatar + '" width="40" height="40" draggable="false">';
+      button.dataset.num = i;
+      fragment.appendChild(button);
+    }
+    var mapPinsBlock = document.querySelector('.map__pins');
+    mapPinsBlock.appendChild(fragment);
+  };
+
+  var removeActivePin = function () {
+    var mapPinActive = document.querySelector('.map__pin--active');
+    if (mapPinActive !== null) {
+      mapPinActive.classList.remove('map__pin--active');
+    }
+  };
 
   // Делаем неактивными все поля формы
 
@@ -116,32 +116,10 @@ window.pin = (function () {
 
   return {
     MAP_PIN_WIDTH_HALF: MAP_PIN_WIDTH_HALF,
-
     MAP_PIN_HEIGHT_PLUS10: MAP_PIN_HEIGHT_PLUS10,
 
     addHandlersForAllButtons: addHandlersForAllButtons,
-
-    removeActivePin: function () {
-      var mapPinActive = document.querySelector('.map__pin--active');
-      if (mapPinActive !== null) {
-        mapPinActive.classList.remove('map__pin--active');
-      }
-    },
-
-    // Функция для создания баттонов для каждого маппина
-
-    addMapPins: function (advertsArray) {
-      var fragment = document.createDocumentFragment();
-      for (var i = 0; i < advertsArray.length; i++) {
-        var button = document.createElement('button');
-        button.style.left = '' + (advertsArray[i].location.x - MAP_PIN_WIDTH_HALF) + 'px';
-        button.style.top = '' + (advertsArray[i].location.y - MAP_PIN_HEIGHT_PLUS10) + 'px';
-        button.className = 'map__pin';
-        button.innerHTML = '<img src="' + advertsArray[i].author.avatar + '" width="40" height="40" draggable="false">';
-        button.dataset.num = i;
-        fragment.appendChild(button);
-      }
-      window.map.mapPinsBlock.appendChild(fragment);
-    }
+    addMapPins: addMapPins,
+    removeActivePin: removeActivePin
   };
 })();
